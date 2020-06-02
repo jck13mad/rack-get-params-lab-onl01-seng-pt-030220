2e0ff75878be92121d5e96ea8e704c0bf8cdb2dd
Jack Madden <jck13mad@gmail.com>
2020-06-02 21:01:42 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -27,7 +27,7 @@ class Application
       item = req.params["item"]
       if @@items.include?(item)
         @@cart << item
-        resp.write ""
+        resp.write "added "
     else
       resp.write "Path Not Found"
     end
