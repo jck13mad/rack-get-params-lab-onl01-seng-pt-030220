0feed6f92a9bcfbbf2cf5736f8c6f68cadef5f47
Jack Madden <jck13mad@gmail.com>
2020-06-02 20:57:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,7 +17,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       unless @@cart.empty?
-        
+        @@cart.each do 
     else
       resp.write "Path Not Found"
     end
